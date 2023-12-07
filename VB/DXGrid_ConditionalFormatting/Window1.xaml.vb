bce4613272c7155b3fbf4157d1c1d005cff498d7
DevExpressExampleBot <fakeEmail>
2023-12-07 18:45:24 +0400

    Source auto update [skip ci]

--- VB/DXGrid_ConditionalFormatting/Window1.xaml.vb
+++ VB/DXGrid_ConditionalFormatting/Window1.xaml.vb
@@ -1,20 +1,14 @@
-﻿Imports System.Collections.Generic
 Imports System.Windows
-Imports System.Windows.Data
-Imports System.Windows.Documents
-Imports System.Windows.Markup
-Imports System.Windows.Media
 Imports System.ComponentModel
-Imports System
-Imports DevExpress.Xpf.Grid
 Imports System.Windows.Controls
 
 Namespace DXGrid_ConditionalFormatting
-    Partial Public Class Window1
+
+    Public Partial Class Window1
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
+            Me.InitializeComponent()
         End Sub
     End Class
 End Namespace
