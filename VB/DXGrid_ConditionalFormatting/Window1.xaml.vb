7cc1810e0f33f733b3f7c763c08167eef59e4c53
DevExpressExampleBot <fakeEmail>
2023-12-07 18:42:58 +0400

    Source auto update [skip ci]

--- VB/DXGrid_ConditionalFormatting/Window1.xaml.vb
+++ VB/DXGrid_ConditionalFormatting/Window1.xaml.vb
@@ -1,7 +1,6 @@
-﻿Imports System.Collections.Generic
+Imports System.Collections.Generic
 Imports System.Windows
 Imports System.Windows.Data
-Imports System.Windows.Documents
 Imports System.Windows.Markup
 Imports System.Windows.Media
 Imports System.ComponentModel
@@ -10,13 +9,13 @@ Imports DevExpress.Xpf.Grid
 Imports System.Windows.Controls
 
 Namespace DXGrid_ConditionalFormatting
-    Partial Public Class Window1
+
+    Public Partial Class Window1
         Inherits Window
 
         Public Sub New()
-            InitializeComponent()
-            grid.ItemsSource = Products.GetData()
-
+            Me.InitializeComponent()
+            Me.grid.ItemsSource = Products.GetData()
         End Sub
     End Class
 
@@ -28,14 +27,13 @@ Namespace DXGrid_ConditionalFormatting
             Return Me
         End Function
 
-        #Region "IMultiValueConverter Members"
-
-        Public Function Convert(ByVal values() As Object, ByVal targetType As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object Implements IMultiValueConverter.Convert
+'#Region "IMultiValueConverter Members"
+        Public Function Convert(ByVal values As Object(), ByVal targetType As Type, ByVal parameter As Object, ByVal culture As Globalization.CultureInfo) As Object Implements IMultiValueConverter.Convert
             If TypeOf values(0) Is GridColumn AndAlso values(1) IsNot Nothing Then
-                Dim column As GridColumn = DirectCast(values(0), GridColumn)
+                Dim column As GridColumn = CType(values(0), GridColumn)
                 Dim cellValue As Object = TypeDescriptor.GetProperties(values(1))(column.FieldName).GetValue(values(1))
-                Dim dict As Dictionary(Of String, ValueState) = DirectCast(TypeDescriptor.GetProperties(values(1))("States").GetValue(values(1)), Dictionary(Of String, ValueState))
-                Dim state As ValueState = Nothing
+                Dim dict As Dictionary(Of String, ValueState) = CType(TypeDescriptor.GetProperties(values(1))("States").GetValue(values(1)), Dictionary(Of String, ValueState))
+                Dim state As ValueState
                 dict.TryGetValue(column.FieldName, state)
                 If state = ValueState.Changed Then
                     Return Brushes.Red
@@ -43,18 +41,20 @@ Namespace DXGrid_ConditionalFormatting
                     Return Brushes.Transparent
                 End If
             End If
+
             Return Nothing
         End Function
 
-        Public Function ConvertBack(ByVal value As Object, ByVal targetTypes() As Type, ByVal parameter As Object, ByVal culture As System.Globalization.CultureInfo) As Object() Implements IMultiValueConverter.ConvertBack
+        Public Function ConvertBack(ByVal value As Object, ByVal targetTypes As Type(), ByVal parameter As Object, ByVal culture As Globalization.CultureInfo) As Object() Implements IMultiValueConverter.ConvertBack
             Throw New NotImplementedException()
         End Function
-
-        #End Region
+'#End Region
     End Class
+
     Public Class Products
+
         Public Shared Function GetData() As List(Of Product)
-            Dim data As New List(Of Product)()
+            Dim data As List(Of Product) = New List(Of Product)()
             data.Add(New Product() With {.ProductName = "Ipoh Coffee", .UnitPrice = 12, .UnitsOnOrder = 2})
             data.Add(New Product() With {.ProductName = "Outback Lager", .UnitPrice = 25, .UnitsOnOrder = 2})
             data.Add(New Product() With {.ProductName = "Boston Crab Meat", .UnitPrice = 18, .UnitsOnOrder = 3332})
@@ -62,46 +62,56 @@ Namespace DXGrid_ConditionalFormatting
             Return data
         End Function
     End Class
+
     Public Enum ValueState
         Original
         Changed
     End Enum
+
     Public Class Product
         Implements INotifyPropertyChanged
 
+        Private _States As Dictionary(Of String, DXGrid_ConditionalFormatting.ValueState)
 
-        Private productName_Renamed As String
-        Public Property ProductName() As String
+        Private productNameField As String
+
+        Public Property ProductName As String
             Get
-                Return productName_Renamed
+                Return productNameField
             End Get
+
             Set(ByVal value As String)
-                If productName_Renamed = value Then
+                If Equals(productNameField, value) Then
                     Return
                 End If
-                If productName_Renamed Is Nothing Then
-                    productName_Renamed = value
+
+                If Equals(productNameField, Nothing) Then
+                    productNameField = value
                     Return
                 End If
-                productName_Renamed = value
+
+                productNameField = value
                 RaisePropertyChanged("ProductName")
                 States("ProductName") = ValueState.Changed
                 RaisePropertyChanged("States")
             End Set
         End Property
 
+        Private unitPriceField As Integer
 
-        Private unitPrice_Renamed As Integer
         Private unitPriceIsAssignedFirstTime As Boolean = True
-        Public Property UnitPrice() As Integer
+
+        Public Property UnitPrice As Integer
             Get
-                Return unitPrice_Renamed
+                Return unitPriceField
             End Get
+
             Set(ByVal value As Integer)
-                If unitPrice_Renamed = value Then
+                If unitPriceField = value Then
                     Return
                 End If
-                unitPrice_Renamed = value
+
+                unitPriceField = value
                 If Not unitPriceIsAssignedFirstTime Then
                     RaisePropertyChanged("UnitPrice")
                     States("UnitPrice") = ValueState.Changed
@@ -112,18 +122,18 @@ Namespace DXGrid_ConditionalFormatting
             End Set
         End Property
 
+        Private unitsOnOrderField As Integer
 
-        Private unitsOnOrder_Renamed As Integer
         Private unitsOnOrderIsAssignedFirstTime As Boolean = True
-        Public Property UnitsOnOrder() As Integer
+
+        Public Property UnitsOnOrder As Integer
             Get
-                Return unitsOnOrder_Renamed
+                Return unitsOnOrderField
             End Get
+
             Set(ByVal value As Integer)
-                If unitsOnOrder_Renamed = value Then
-                    Return
-                End If
-                unitsOnOrder_Renamed = value
+                If unitsOnOrderField = value Then Return
+                unitsOnOrderField = value
                 If Not unitsOnOrderIsAssignedFirstTime Then
                     RaisePropertyChanged("UnitsOnOrder")
                     States("UnitsOnOrder") = ValueState.Changed
@@ -134,26 +144,24 @@ Namespace DXGrid_ConditionalFormatting
             End Set
         End Property
 
-
-        Private privateStates As Dictionary(Of String, ValueState)
-        Public Property States() As Dictionary(Of String, ValueState)
+        Public Property States As Dictionary(Of String, ValueState)
             Get
-                Return privateStates
+                Return _States
             End Get
+
             Private Set(ByVal value As Dictionary(Of String, ValueState))
-                privateStates = value
+                _States = value
             End Set
         End Property
+
         Public Sub New()
             States = New Dictionary(Of String, ValueState)()
         End Sub
 
-        #Region "INotifyPropertyChanged Members"
-
+'#Region "INotifyPropertyChanged Members"
         Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
 
-        #End Region
-
+'#End Region
         Private Sub RaisePropertyChanged(ByVal propertyName As String)
             RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(propertyName))
         End Sub
