5783a469aece881eec1cab3e208aab58f6f6bdf7
AndreySlabov <andrey.slabov@devexpress.com>
2023-12-07 18:03:27 +0400

    Format Example

--- CS/DXGrid_ConditionalFormatting/ViewModel.cs
+++ CS/DXGrid_ConditionalFormatting/ViewModel.cs
@@ -1,18 +1,12 @@
-﻿using DevExpress.Mvvm.DataAnnotations;
+﻿using DevExpress.Mvvm;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
 using System.Windows.Threading;
 
 namespace DXGrid_ConditionalFormatting {
-    public class ViewModel {
-        public ObservableCollection<Product> Products {
-            get;
-            set;
-        }
+    public class ViewModel : ViewModelBase {
+        public ObservableCollection<Product> Products { get; set; }
+
         DispatcherTimer updateTimer = new DispatcherTimer();
         public ViewModel() {
             PopulateData();
@@ -24,7 +18,7 @@ namespace DXGrid_ConditionalFormatting {
             for (int i = 0; i < 30; i++) {
                 decimal oldPrice = ((decimal)randomPriceValue.Next(1000) / 10);
                 decimal newPrice = ((decimal)randomPriceValue.Next(10000) / 10);
-                Products.Add(new Product() { Name = "Product" + i, OldPrice = oldPrice, NewPrice=newPrice, DeltaPrice = newPrice - oldPrice,  IsAvailable = (i % 2 == 0) });
+                Products.Add(new Product() { Name = "Product" + i, OldPrice = oldPrice, NewPrice = newPrice, DeltaPrice = newPrice - oldPrice, IsAvailable = (i % 2 == 0) });
             }
         }
         private void SetUpdateTimer() {
@@ -41,7 +35,7 @@ namespace DXGrid_ConditionalFormatting {
             }
             for (int i = 0; i < 5; i++) {
                 updateProductNumber = rnd.Next(Products.Count);
-              
+
                 decimal oldPrice = ((decimal)rnd.Next(1000) / 10);
                 decimal newPrice = ((decimal)rnd.Next(10000) / 10);
 
@@ -53,70 +47,11 @@ namespace DXGrid_ConditionalFormatting {
         }
     }
 
-    public class Product : INotifyPropertyChanged {
-        string name;
-        decimal oldPrice;
-        decimal newPrice;
-        decimal deltaPrice;
-        bool isAvailable;
-
-        public string Name {
-            get {
-                return name;
-            }
-            set {
-                name = value;
-                RaisePropertyChanged("Name");
-            }
-        }
-        public decimal OldPrice {
-            get {
-                return oldPrice;
-            }
-            set {
-                oldPrice = value;
-                RaisePropertyChanged("OldPrice");
-            }
-        }
-        public decimal NewPrice
-        {
-            get
-            {
-                return newPrice;
-            }
-            set
-            {
-                newPrice = value;
-                RaisePropertyChanged("NewPrice");
-            }
-        }
-        public decimal DeltaPrice
-        {
-            get
-            {
-                return deltaPrice;
-            }
-            set
-            {
-                deltaPrice = value;
-                RaisePropertyChanged("DeltaPrice");
-            }
-        }
-        public bool IsAvailable {
-            get {
-                return isAvailable;
-            }
-            set {
-                isAvailable = value;
-                RaisePropertyChanged("IsAvailable");
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected void RaisePropertyChanged(string propertyName) {
-            if (PropertyChanged != null)
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-        }
+    public class Product : BindableBase {
+        public string Name { get { return GetValue<string>(); } set { SetValue(value); } }
+        public decimal OldPrice { get { return GetValue<decimal>(); } set { SetValue(value); } }
+        public decimal NewPrice { get { return GetValue<decimal>(); } set { SetValue(value); } }
+        public decimal DeltaPrice { get { return GetValue<decimal>(); } set { SetValue(value); } }
+        public bool IsAvailable { get { return GetValue<bool>(); } set { SetValue(value); } }
     }
 }
--- CS/DXGrid_ConditionalFormatting/Window1.xaml.cs
+++ CS/DXGrid_ConditionalFormatting/Window1.xaml.cs
@@ -1,13 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Windows;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Markup;
-using System.Windows.Media;
-using System.ComponentModel;
-using System;
-using DevExpress.Xpf.Grid;
-using System.Windows.Controls;
+﻿using System.Windows;
 
 namespace DXGrid_ConditionalFormatting {
     public partial class Window1 : Window {
